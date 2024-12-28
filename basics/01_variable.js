b968cbe898e6ff9ec19eae229072edaa0945ea3a
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-12-28 13:56:01 +0530

    variale

---
+++ basics/01_variable.js
@@ -0,0 +1,6 @@
+const accounId = 144553
+let accountEmail = "devarshi@google.com"
+var accounPassword = "1234"
+accountCity = "jaipur"
+
+console.table([accounId, accountEmail, accounPassword, accountCity])
