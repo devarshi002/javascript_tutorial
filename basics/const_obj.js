b7f30818b08f3aff491d2b699b782debdbf33474
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-12-28 14:56:50 +0530

    constant

---
+++ basics/const_obj.js
@@ -0,0 +1,12 @@
+const car = {type:'fiat', model:'500', color:'white'}
+
+//you can change a property
+car.color = 'red'
+
+
+// you can add a property
+car.owner = "johnson"
+
+console.log(car)
+
+//but you cannot resign the object
